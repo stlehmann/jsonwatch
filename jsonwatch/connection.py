b8362a0e4f4bdbfe1ddabd6a4276c377ab01f11a
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2016-09-23 21:36:42 +0200

    send sample data from testserver

--- jsonwatch/connection.py
+++ jsonwatch/connection.py
@@ -6,8 +6,13 @@ Module for diffent connections.
 :email: stefan.st.lehmann@gmail.com
 
 """
+import logging
+import threading
 from serial import Serial
 import socket
+
+
+logger = logging.getLogger(__name__)
 
 
 class Connection:
@@ -31,20 +36,44 @@ class SerialConnection(Connection):
     def receive(self, size=1):
         return self.serial.read(size)
 
+    @property
+    def connected(self):
+        return self.serial.is_open()
+
 
 class SocketConnection(Connection):
     def __init__(self):
         super().__init__()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._connected = False
 
     def connect(self, host=None, port=None):
         self.socket.connect((host, port))
+        self._connected = True
 
     def disconnect(self):
         self.socket.close()
+        self._connected = False
 
     def send(self, data: bytes):
         return self.socket.send(bytes)
 
     def receive(self):
         return self.socket.recvfrom(4096)
+
+    @property
+    def connected(self):
+        return self._connected
+
+
+class ConnectionThread(threading.Thread):
+    def __init__(self, connection, *args, **kwargs):
+        self.connection = connection
+        self._run = True
+
+        super(ConnectionThread, self).__init__(*args, **kwargs)
+
+    def run(self):
+        while self._run:
+            data = self.connection.receive()
+            logger.info('incoming message: {0}'.format(data))
